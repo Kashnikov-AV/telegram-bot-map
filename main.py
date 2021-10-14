5aecb8712a369e9c1cef132c3b06ade1fd04411b
aleks <duo1245@yandex.ru>
2021-10-14 17:54:11 +0300

    first

--- main.py
+++ main.py
@@ -6,5 +6,9 @@ token = "2069589275:AAEV-G8mZtYZR4P_W7O7ZFFc9zqnw6zbw50"
 
 bot = telebot.TeleBot(token)
 
+@bot.message_handler(commands=['start'])
+def start_message(message):
+    return bot.send_message(message.chat.id, "привет пользователь, этот бот сохраняет твои любимые места")
 
-
+if __name__ == '__main__':
+    bot.polling()
